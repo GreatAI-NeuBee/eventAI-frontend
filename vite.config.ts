f59f9cc845bba4df2f7ec97803745e5275680e6d
Kok Jia Yin <jiayinkok@gmail.com>
2025-09-21 20:15:26 +0800

    Update config - console

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    // Cast to any for typescript used
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    } as any,
+  },
 })
